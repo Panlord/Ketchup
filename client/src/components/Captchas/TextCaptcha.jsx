5d539f8ff3853a0b9cd6b3717b5e848a4642ccf4
Panlord <adpan.dev@gmail.com>
2022-08-09 20:03:03 -0700

    style: lay out text captcha

--- client/src/components/Captchas/TextCaptcha.jsx
+++ client/src/components/Captchas/TextCaptcha.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
+import styled from 'styled-components';
 
 class TextCaptcha extends React.Component {
   constructor(props) {
@@ -28,12 +28,53 @@ class TextCaptcha extends React.Component {
 
   render () {
     return (
-      <div>
-        Text Captcha Goes here.
-        <img src={this.state.captchaImgSrc} />
-      </div>
+      <TextCaptchaContainer>
+        <CaptchaImage src={this.state.captchaImgSrc} />
+        <BottomContainer>
+          <AnswerInputContainer>
+            <AnswerInputLabel>Type the characters you see in the picture:</AnswerInputLabel>
+            <AnswerInputField type="text" />
+          </AnswerInputContainer>
+          <SubmitButton>Submit</SubmitButton>
+        </BottomContainer>
+      </TextCaptchaContainer>
     );
   }
 }
+
+// Styled components
+const TextCaptchaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #8c030a;
+  padding: 5px;
+  width: 402px;
+  border-radius: 8px;
+`;
+const CaptchaImage = styled.img`
+  border-radius: 4px;
+`;
+const BottomContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-top: 2px;
+`;
+const AnswerInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #e6d375;
+  border-radius: 4px;
+  padding: 4px;
+  margin: 2px;
+`;
+const AnswerInputLabel = styled.span`
+
+`;
+const AnswerInputField = styled.input`
+  border-radius: 2px;
+`;
+const SubmitButton = styled.button`
+  border-radius: 4px;
+`;
 
 export default TextCaptcha;
